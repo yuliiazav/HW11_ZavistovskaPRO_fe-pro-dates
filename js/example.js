db316670ed403a8c677466a8c48cfaa635e18eb6
Bohdan Onatskyi <15bogdan17@gmail.com>
2022-02-02 23:25:21 +0000

    add examples

---
+++ js/example.js
@@ -0,0 +1,6 @@
+console.log(getDay(new Date(2022, 1, 1), 'ru')); // Вторник;
+console.log(getDay(new Date(2022, 1, 1), 'en')); // Tuesday;
+
+console.log(formatTime(new Date(2021, 1, 1, 20, 1))); // 20:01
+
+console.log(getLastDayOfMonth(2022, 1)); // 28
