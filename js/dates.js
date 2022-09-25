b1d61168b65087c02313ad418f778f60422ec061
Your Nameyuliiazav <didiky1993@gmail.com>
2022-09-25 19:16:34 +0300

    added HW files dates

--- js/dates.js
+++ js/dates.js
@@ -2,6 +2,8 @@
 // мы передаем объект даты и вторым аргументом язык(доступно либо 'en' либо 'ru')
 // и функция должна вернуть день недели словом в зависимости от языка. Очень круто,
 // если выйдет сделать это ОДНОЙ строкой.
+let date = new Date();
+
 function getDay(date, lang) {
   const dayNames = {
     en: {
@@ -23,12 +25,31 @@ function getDay(date, lang) {
       6: 'Суббота',
     },
   };
+  return dayNames[lang][date.getDay()];
 }
 
 // Принимает объект даты, и должно вернуть компоненты даты в виде строки.
 // Вид должен быть такой 12:02(часы и минуты), то есть если у вас одно число на одном из
 // компонентов, то добавляем 0 перед ним
-function formatTime(date) {}
+
+function formatTime(date) {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let result = `${hours} : ${minutes}`;
+
+    if (hours < 10 && minutes >= 10) {
+    return `0${hours} : ${minutes}`
+  }
+    if (hours >= 10 && minutes < 10) {
+    return `${hours} : 0${minutes}`
+  }
+  if (hours < 10 && minutes < 10) {
+    return `0${hours} : 0${minutes}`
+  }  else {
+    return result;
+  }
+
+}
 
 /*
 Напишите функцию getLastDayOfMonth(year, month), 
@@ -39,7 +60,15 @@ year – год из четырёх цифр, например, 2012.
 month – месяц от 0 до 11.
 К примеру, getLastDayOfMonth(2012, 1) = 29 (високосный год, февраль).
 */
-function getLastDayOfMonth(year, month) {}
+// function getLastDayOfMonth(year, month) {
+//   let date = new Date(year, month + 1, 0);
+//   return date.getDate();
+// }
+
+function getLastDayOfMonth(year, month) {
+let date = new Date(year, month+1, 0);
+ return date.getDate();
+}
 
 module.exports = {
   getDay,
--- js/example.js
+++ js/example.js
@@ -2,5 +2,7 @@ console.log(getDay(new Date(2022, 1, 1), 'ru')); // Вторник;
 console.log(getDay(new Date(2022, 1, 1), 'en')); // Tuesday;
 
 console.log(formatTime(new Date(2021, 1, 1, 20, 1))); // 20:01
+console.log(formatTime(new Date(2021, 1, 1, 20, 4))); // 20:01
+console.log(formatTime(new Date(2021, 1, 1, 14, 2))); // 20:01
 
 console.log(getLastDayOfMonth(2022, 1)); // 28
