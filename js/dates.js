c6aa49d1c755541c5b6706e5449ff373325bee4c
Your Nameyuliiazav <didiky1993@gmail.com>
2022-09-25 19:19:14 +0300

    added files

--- js/dates.js
+++ js/dates.js
@@ -60,10 +60,6 @@ year – год из четырёх цифр, например, 2012.
 month – месяц от 0 до 11.
 К примеру, getLastDayOfMonth(2012, 1) = 29 (високосный год, февраль).
 */
-// function getLastDayOfMonth(year, month) {
-//   let date = new Date(year, month + 1, 0);
-//   return date.getDate();
-// }
 
 function getLastDayOfMonth(year, month) {
 let date = new Date(year, month+1, 0);
